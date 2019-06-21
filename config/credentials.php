fc1aaa6445c873fe713fa56761d28c62bc2ac905
ii02735 <33719388+ii02735@users.noreply.github.com>
2019-06-21 17:16:20 +0200

    Update credentials.php

--- config/credentials.php
+++ config/credentials.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    "driver" => "pdo_mysql",
-    "user" => "ada_fr",
-    "password" => "ada_db_pass",
-    "host" => "localhost",
-    "dbname" => "ada_fr_db_2"
+    "driver" => "",
+    "user" => "",
+    "password" => "",
+    "host" => "",
+    "dbname" => ""
 ];
