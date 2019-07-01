5c4665e3a967b2505e689a3812e31276199bf961
ii02735 <33719388+ii02735@users.noreply.github.com>
2019-07-01 11:54:30 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -46,4 +46,4 @@ $basePath = null;
 $lang = $_ENV["CRON_SCHEDULER_LANG"];
 
 //File that must be loaded for user's choices (cron:scheduler:add)
-$baseFile = __DIR__."/../src/commands.yml";
+$baseFile = $_ENV["CRON_BASE_FILE"];
